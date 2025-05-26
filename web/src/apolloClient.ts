baa005818be7f60e7ea75cebc1cb1923a7712401
Jess Sevilleja <jmvsevilleja@gmail.com>
2025-05-26 12:11:17 +0800

    updated vite env

--- web/src/apolloClient.ts
+++ web/src/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.REACT_APP_API_URL || "http://localhost:3001/graphql",
+  uri: import.meta.env.VITE_APP_API_URL || "http://localhost:3001/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
